9fd7297a74166fe815850b96371fdd665eb479c9
Alexander Ziskind <alex@nuvious.com>
2019-02-04 16:03:58 -0500

    moved modal styles to global css

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,9 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import SideMenu from '@/components/SideMenu.vue';
-import MainMenu from '@/components/MainMenu.vue';
+import { Component, Vue } from "vue-property-decorator";
+import SideMenu from "@/components/SideMenu.vue";
+import MainMenu from "@/components/MainMenu.vue";
 
 /*
 Component.registerHooks([
@@ -30,8 +30,8 @@ Component.registerHooks([
 @Component({
   components: {
     MainMenu,
-    SideMenu,
-  },
+    SideMenu
+  }
 })
 export default class App extends Vue {}
 </script>
@@ -201,5 +201,62 @@ export default class App extends Vue {}
 
 .logo {
   width: 100px;
+}
+
+/* modal */
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 500px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
 }
 </style>
