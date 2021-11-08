6ca8ca672dfba38495730592646fe19d0570b0fa
zdravkov <zdravkov.pl@gmail.com>
2021-11-08 17:01:54 +0200

    chore: Add chart combo and buttons

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -14,21 +14,28 @@
 
       <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group mr-2">
-          <button
-            type="button"
-            class="btn btn-sm btn-outline-secondary"
-            @click="(e) => onMonthRangeTap(3)"
-          >3 Months</button>
-          <button
-            type="button"
-            class="btn btn-sm btn-outline-secondary"
-            @click="(e) => onMonthRangeTap(6)"
-          >6 Months</button>
-          <button
-            type="button"
-            class="btn btn-sm btn-outline-secondary"
-            @click="(e) => onMonthRangeTap(12)"
-          >1 Year</button>
+          <kendo-combobox
+            :data-items="users"
+            :value="selectedUserIdStr"
+            :placeholder="'Select assignee...'"
+            @open="userFilterOpen"
+            :item-render="'myTemplate'"
+            @change="userFilterValueChange"
+            :style="{width: '250px'}"
+          >
+            <template v-slot:myTemplate="{props}">
+              <div class="row k-item" style="margin-left: 5px;" @click="(ev) => props.onClick(ev)">
+                <img class="li-avatar rounded mx-auto d-block" :src="props.dataItem.avatar" />
+                <span style="margin-left: 5px;">{{ props.dataItem.fullName }}</span>
+            </div>
+            </template>
+          </kendo-combobox>
+
+          <kendo-buttongroup>
+            <kendo-button :togglable="true" :look="'flat'" :icon="'calendar'"  @click="onMonthRangeTap(3)"> 3 Months </kendo-button>
+            <kendo-button :togglable="true" :look="'flat'" :icon="'calendar'"   @click="onMonthRangeTap(6)"> 6 Months </kendo-button>
+            <kendo-button :togglable="true" :look="'flat'"  :icon="'calendar'"   @click="onMonthRangeTap(12)"> 1 Year </kendo-button>
+          </kendo-buttongroup>
         </div>
       </div>
     </div>
@@ -36,11 +43,33 @@
     <div class="card">
       <h3 class="card-header">Active Issues</h3>
       <div class="card-block">
-        <ActiveIssues :statusCounts="statusCounts"/>
+        <ActiveIssues :statusCounts="statusCounts" />
 
         <div class="row">
           <div class="col-sm-12">
             <h3>All issues</h3>
+             <kendo-chart :theme="'sass'">
+              <ChartTitle
+                :text="'All Issues'"
+                :position="'top'"
+                :align="'center'"
+              />
+              <ChartCategoryAxis>
+                <ChartCategoryAxisItem
+                  :categories="categories"
+                  :axis-crossing-value="[0, 20]"
+                  :axis-major-grid-lines-visible="false"
+                  :axis-labels-rotation="'auto'"
+                  :axis-base-unit="'months'"
+                />
+              </ChartCategoryAxis>
+              <ChartLegend :position="'bottom'" />
+              <ChartSeriesDefaults :type="'column'" :stack="true" :gap="0.06" />
+              <ChartSeries>
+                <ChartSeriesItem v-bind="series[0]" />
+                <ChartSeriesItem v-bind="series[1]" />
+              </ChartSeries>
+            </kendo-chart>
           </div>
         </div>
       </div>
@@ -49,19 +78,36 @@
 </template>
 
 <script lang="ts">
+import { Observable } from 'rxjs';
+
+import { PtItem, PtUser } from '@/core/models/domain';
+import { Store } from '@/core/state/app-store';
 import {
-  DashboardRepository,
-  DashboardFilter,
-  FilteredIssues,
-} from "@/repositories/dashboard-repository";
-import { DashboardService } from "@/services/dashboard-service";
+    DashboardRepository,
+    DashboardFilter,
+    FilteredIssues,
+} from '@/repositories/dashboard-repository';
+import { DashboardService } from '@/services/dashboard-service';
 import {
     TypeCounts,
     PriorityCounts,
     StatusCounts,
-} from "@/shared/models/ui/stats";
-import { formatDateEnUs } from "@/core/helpers/date-utils";
-import ActiveIssues from "@/components/dashboard/ActiveIssues.vue";
+} from '@/shared/models/ui/stats';
+import { formatDateEnUs } from '@/core/helpers/date-utils';
+import ActiveIssues from '@/components/dashboard/ActiveIssues.vue';
+import { PtUserService } from '@/core/services/pt-user-service';
+import { ComboBox } from "@progress/kendo-vue-dropdowns";
+import { Button, ButtonGroup } from "@progress/kendo-vue-buttons";
+import {
+  Chart,
+  ChartSeries,
+  ChartSeriesItem,
+  ChartTitle,
+  ChartLegend,
+  ChartCategoryAxis,
+  ChartCategoryAxisItem,
+  ChartSeriesDefaults,
+} from "@progress/kendo-vue-charts";
 
 interface DateRange {
   dateStart: Date;
@@ -74,6 +120,17 @@ export default defineComponent({
   name: "DashboardPage",
   components: {
     ActiveIssues,
+    "kendo-combobox": ComboBox,
+    "kendo-button": Button,
+    "kendo-buttongroup": ButtonGroup,
+    "kendo-chart": Chart,
+    ChartSeries,
+    ChartSeriesItem,
+    ChartTitle,
+    ChartLegend,
+    ChartCategoryAxis,
+    ChartCategoryAxisItem,
+    ChartSeriesDefaults,
   },
   setup() {
     const filter = ref<DashboardFilter>({});
@@ -86,18 +143,77 @@ export default defineComponent({
     const categories = ref<Date[]>([]);
     const itemsOpenByMonth = ref<number[]>([]);
     const itemsClosedByMonth = ref<number[]>([]);
+    const series = ref([
+      {
+        name: 'Open',
+        'data-items': [],
+        color: '#CC3458',
+        opacity: 0.7
+      },
+      {
+        name: 'Closed',
+        'data-items': [],
+        color: '#35C473',
+        opacity: 0.7
+      },
+    ]);
+    // public itemsOpenByMonth: number[] = [];
+    // public itemsClosedByMonth: number[] = [];
 
+    const store: Store = new Store();
     const dashboardRepo: DashboardRepository = new DashboardRepository();
     const dashboardService: DashboardService = new DashboardService(
       dashboardRepo
     );
 
+    const userService: PtUserService = new PtUserService(store);
+    const users = ref<PtUser[]>([]);
+    const users$: Observable<PtUser[]> = store.select<PtUser[]>('users');
+    const selectedUserIdStr = ref("");
     const refresh = () => {
-      dashboardService.getStatusCounts(filter.value as DashboardFilter).then((result) => {
-        statusCounts.value = result;
+      Promise.all<StatusCounts, FilteredIssues>([
+        dashboardService.getStatusCounts(filter.value as any),
+        dashboardService.getFilteredIssues(filter.value as any),
+      ]).then(results => {
+        statusCounts.value = results[0];
+        updateStats(results[1]);
       });
     };
+
+    const updateStats = (issuesAll: FilteredIssues) => {
+      const cats = issuesAll.categories.map(c => new Date(c));
+
+      const itemsOpenByMonth: number[] = [];
+      const itemsClosedByMonth: number[] = [];
+
+      issuesAll.items.forEach((item, index) => {
+        itemsOpenByMonth.push(item.open.length);
+        itemsClosedByMonth.push(item.closed.length);
+      });
+
+      categories.value = cats;
+      series.value[0]['data-items'] = itemsOpenByMonth as any;
+      series.value[1]['data-items'] = itemsClosedByMonth as any;
+    };
+
+    const userFilterOpen = () => {
+      userService.fetchUsers();
+    };
+
+    users$.subscribe((newUsers: PtUser[]) => {
+      users.value = newUsers;
+    });
     refresh();
+
+    const userFilterValueChange = (e: any) => {
+      selectedUserIdStr.value = e.value ? e.value.id : "";
+      if (selectedUserIdStr.value) {
+        filter.value.userId = Number(selectedUserIdStr.value);
+      } else {
+        filter.value.userId = undefined;
+      }
+      refresh();
+    };
 
     const onMonthRangeTap = (months: number) => {
       const range = getDateRange(months);
@@ -128,7 +244,12 @@ export default defineComponent({
       categories,
       itemsOpenByMonth,
       itemsClosedByMonth,
+      series,
+      userFilterValueChange,
+      users,
+      userFilterOpen,
+      selectedUserIdStr,
     };
-  }
+  },
 });
 </script>
